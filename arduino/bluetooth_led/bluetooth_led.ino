ec64ad89cdf2c3d2b463ebdc52c996c8abd31033
Sean Olejar <solejar236@gmail.com>
2018-03-04 15:49:56 -0500

    first iteration of bluetooth

--- arduino/bluetooth_led/bluetooth_led.ino
+++ arduino/bluetooth_led/bluetooth_led.ino
@@ -1,9 +1,57 @@
+cjar command;
+String string;
+boolean ledon = false;
+#define led 5
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available()>0){
+    string = "";
+  }
+  while(Serial.availble()>0{
+    command = ((byte)Serial.read());
+    if(command==":"){
+      break;
+    }else{
+      string += command;
+    }
 
+    delay(1);
+  }
+
+  if(string == "TO"){
+    ledOn();
+    ledon = true;
+  }
+  if(string=="TF"){
+    ledOff();
+    ledon = false;
+    Serial.println(string);
+  }
+
+  if((string.toInt()>=0)&&(string.toInt()<=255)){
+    if(ledon==true){
+      analogWrite(led, string.toInt());
+      Serial.println(string); //debug
+      delay(10);
+    }
+  }
 }
+
+void ledOn(){
+  analogWrite(led,255);
+  delay(10);
+}
+
+void ledOff(){
+  analogWrite(led,0);
+  delay(10);
+}
+
+
